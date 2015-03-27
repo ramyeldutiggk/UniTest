7095e93c80263c2bf6b17cf54536dcded54289a2
NikiLinux <nikmic1994@hotmail.com>
2015-03-27 21:42:52 +0100

    PLEASE2

--- src/main/java/sourceCode/App.java
+++ src/main/java/sourceCode/App.java
@@ -12,12 +12,12 @@ public class App
     public static char test3(){
         return 'a';
     }
-    public static void main( String[] args )
+    /*public static void main( String[] args )
     {
         System.out.println(test1());
         System.out.println(test2());
         System.out.println(test3());
         
         System.out.println( "Hello World!" );
-    }
+    }*/
 }
