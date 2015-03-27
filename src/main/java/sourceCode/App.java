1c800de13ee2976d73c148c64bd70127e8b7dfb0
camillu95 <malcolmcamilleri95@hotmail.com>
2015-03-27 21:15:56 +0100

    File path alteration

---
+++ src/main/java/sourceCode/App.java
@@ -0,0 +1,23 @@
+package sourceCode;
+
+public class App 
+{
+    
+    public static int test1(){
+        return 0;
+    }
+    public static int test2(){
+        return 1;
+    }
+    public static char test3(){
+        return 'a';
+    }
+    public static void main( String[] args )
+    {
+        System.out.println(test1());
+        System.out.println(test2());
+        System.out.println(test3());
+        
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/sourceCode/AppTest.java
@@ -0,0 +1,33 @@
+package sourceCode;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class AppTest 
+{
+   final App a = new App();
+   
+   @Test
+    public void t1() {
+        final int t1 = a.test1();
+        Assert.assertEquals(0, t1);
+    }
+    
+    @Test
+    public void t2() {
+        final int t2 = a.test2();
+        Assert.assertEquals(0, t2);
+    }
+    
+    @Test
+    public void t3() {
+        final int t3 = a.test3();
+        Assert.assertEquals('a', t3); 
+    }
+    
+    @Test
+    public void t4() {
+        final int t3 = a.test3();
+        Assert.assertEquals('b', t3);
+    }
+}
