12595a1d2d17a16a31294042eb94fcbc39b057aa
NikiLinux <nikmic1994@hotmail.com>
2015-04-17 18:48:09 +0200

    1 more test when source == destination. 100% coverage potentially.

--- src/main/java/sourceCode/TransactionManager.java
+++ src/main/java/sourceCode/TransactionManager.java
@@ -15,35 +15,33 @@ public class TransactionManager {
         int counter;
         long timeTemp = System.currentTimeMillis();
         Timings t = new Timings(src, dsc, timeTemp);
-
-        if (src == dsc) {
+        
+        if(src == dsc){
             System.out.println("The source and destination accounts can't be the same.\n");
-            return false;
+            return false;} 
+
+        for (counter = transactionTime.size() - 1; counter >= 0; counter--) {
+            if (timeTemp - transactionTime.get(counter).getTime() > 15000) {
+                transactionTime.remove(counter);
+                continue;
+            }
+
+            if (transactionTime.get(counter).getSourceAccountNumber() == src || transactionTime.get(counter).getSourceAccountNumber() == dsc || transactionTime.get(counter).getDestinationAccountNumber() == dsc || transactionTime.get(counter).getDestinationAccountNumber() == src) {
+                if ((timeTemp - transactionTime.get(counter).getTime()) < 15000) {
+                    System.out.println("One of the accounts being used in this transaction was already used in another transaction less than 15 seconds ago!\n");
+                    System.out.println("Transaction failed!\n");
+                    return false;
+                }
+            }
+        }
+
+        if (t1.process()) {
+            transactionTime.add(t);
+            System.out.println("Transaction complete!\n");
+            numTransactionsProcessed++;
+            return true;
         } else {
-            for (counter = transactionTime.size() - 1; counter >= 0; counter--) {
-                if (timeTemp - transactionTime.get(counter).getTime() > 15000) {
-                    transactionTime.remove(counter);
-                    continue;
-                }
-
-                if (transactionTime.get(counter).getSourceAccountNumber() == src || transactionTime.get(counter).getSourceAccountNumber() == dsc || transactionTime.get(counter).getDestinationAccountNumber() == dsc || transactionTime.get(counter).getDestinationAccountNumber() == src) {
-                    if ((timeTemp - transactionTime.get(counter).getTime()) < 15000) {
-                        System.out.println("One of the accounts being used in this transaction was already used in another transaction less than 15 seconds ago!\n");
-                        System.out.println("Transaction failed!\n");
-                        return false;
-                    }
-                }
-            }
-
-            if (t1.process()) {
-                transactionTime.add(t);
-                System.out.println("Transaction complete!\n");
-                numTransactionsProcessed++;
-                return true;
-            } else {
-                return false;
-            }
-
+            return false;
         }
     }
 }
