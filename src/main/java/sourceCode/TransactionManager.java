4ba949000badcc3c204f14acdea156cd48b7aabf
NikiLinux <nikmic1994@hotmail.com>
2015-04-19 00:42:41 +0200

    1 more test when source == destination. 100% coverage potentially.

--- src/main/java/sourceCode/TransactionManager.java
+++ src/main/java/sourceCode/TransactionManager.java
@@ -17,23 +17,23 @@ public class TransactionManager {
         Timings t = new Timings(src, dsc, timeTemp);
         
         if(src == dsc){
-            System.out.println("1");
+            //System.out.println("1");
             System.out.println("The source and destination accounts can't be the same.\n");
             return false;
         }
 
         for (int counter = transactionTime.size() - 1; counter >= 0; counter--) {
-            System.out.println("2");
+            //System.out.println("2");
             if (timeTemp - transactionTime.get(counter).getTime() > 15000) {
-                System.out.println("3");
+                //System.out.println("3");
                 transactionTime.remove(counter);
                 continue;
             }
 
             if (transactionTime.get(counter).getSourceAccountNumber() == src || transactionTime.get(counter).getSourceAccountNumber() == dsc || transactionTime.get(counter).getDestinationAccountNumber() == dsc || transactionTime.get(counter).getDestinationAccountNumber() == src) {
-                System.out.println("4");
+                //System.out.println("4");
                 if ((timeTemp - transactionTime.get(counter).getTime()) < 15000) {
-                    System.out.println("5");
+                    //System.out.println("5");
                     System.out.println("One of the accounts being used in this transaction was already used in another transaction less than 15 seconds ago!\n");
                     System.out.println("Transaction failed!\n");
                     return false;
@@ -42,13 +42,13 @@ public class TransactionManager {
         }
 
         if (t1.process()) {
-            System.out.println("6");
+            //System.out.println("6");
             transactionTime.add(t);
             System.out.println("Transaction complete!\n");
             numTransactionsProcessed++;
             return true;
         } else {
-            System.out.println("7");
+            //System.out.println("7");
             return false;
         }
     }
