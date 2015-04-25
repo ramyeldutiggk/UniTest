33f922bc3c91540b284281871c986b6ea3ed8603
camillu95 <malcolmcamilleri95@hotmail.com>
2015-04-25 19:00:36 +0200

    Edited account class

--- src/main/java/sourceCode/Account.java
+++ src/main/java/sourceCode/Account.java
@@ -10,8 +10,8 @@ public class Account {
     private String accountName;
     private long accountBalance;
 
-    boolean adjustBalance(long ammount) {
-        long newBalance = accountBalance + ammount;
+    boolean adjustBalance(long amount) {
+        long newBalance = accountBalance + amount;
 
         if (newBalance >= 0) {
             accountBalance = newBalance;
